918accfb617769eb8e88b0ba47a52b3ca44465c6
Bilgehan Kalay <kalaybilgehan60@gmail.com>
2024-10-11 10:24:01 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,5 +10,11 @@ BACKUP_DIR="/backup"
 # MongoDB dump komutunu çalıştırıyoruz
 mongodump --host mongo --out $BACKUP_DIR/mongo-backup-$BACKUP_DATE
 
+if [ $? -eq 0 ]; then
+  echo "Yedekleme başarılı: $BACKUP_DIR/mongo-backup-$BACKUP_DATE"
+else
+  echo "Yedekleme sırasında bir hata oluştu"
+fi
+
 # Eski yedekleri silmek için örneğin 7 günden eski dosyaları temizleyebiliriz
 find $BACKUP_DIR -type d -mtime +7 -exec rm -rf {} \;
